f74de96b3ebeecc40a8443b5682acb585ca843af
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2023-03-01 09:49:09 +0300

    grt locale from request

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+'''
+Get Locale from request
+'''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    '''
+    config class
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object('2-app.Config')
+
+
+@babel.localeselector
+def get_locale():
+    '''
+    Determine best match with supported languages
+    '''
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def home() -> str:
+    '''
+    route to 2-index.html
+    '''
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
