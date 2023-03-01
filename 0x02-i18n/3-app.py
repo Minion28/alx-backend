8f8fedda4327e3212afece28a2a93c201e7be3b0
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2023-03-01 10:03:42 +0300

    parameterize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+'''
+Parameterize templates
+'''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    '''
+    config class
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object('3-app.Config')
+
+
+@babel.localeselector
+def get_locale():
+    '''
+    Determine best match with supported languages
+    '''
+    return request.accept_languages.best_match(app.Config['LANGUAGES'])
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def hello_world() -> str:
+    '''
+    return 3-index.html
+    '''
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
